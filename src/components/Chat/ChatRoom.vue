57480e37660173bf260ea619bc1de84ee5ab73ce
JaoJaoLin <jame31560@gmail.com>
2020-03-16 17:53:41 +0800

    create chat

---
+++ src/components/Chat/ChatRoom.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="chatroom">
+    <a href="javascript:;">{{roomid}}</a>
+    <input
+      type="text"
+      v-model="roomid"
+    >
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      roomid: String
+    }
+  };
+</script>
---
+++ src/views/Chat.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="chat">
+    <ChatRoom roomid="123" />
+  </div>
+
+</template>
+
+<script>
+  import ChatRoom from "@/components/Chat/ChatRoom.vue";
+
+  export default {
+    name: "home",
+    components: {
+      ChatRoom
+    }
+  };
+</script>
